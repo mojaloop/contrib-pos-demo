192310e97a0f45b49642a9c7d23ec397840d9e7a
Juan Correa <gibaros@users.noreply.github.com>
2025-03-17 12:44:34 -0400

    chore: revert automated change

--- ISO-Open API/Open API Adapter/iso-to-openapi.js
+++ ISO-Open API/Open API Adapter/iso-to-openapi.js
@@ -32,18 +32,8 @@ async function handleISO(data, sock) {
     if (isopack1.validateMessage()) {
         console.log(` iso message type: ${isoUnpacked[0]}`)
 
-        //*****
-License
---------------
-Copyright © 2020-2025 Mojaloop Foundation
-The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*****/
-
-//isoUnpacked[30] fees=
+        //*******************************************************************/
+        //isoUnpacked[30] fees=
 
         let amtIso = (+isoUnpacked[4] / 100).toFixed(2);
         let amtStr = amtIso.toString();
